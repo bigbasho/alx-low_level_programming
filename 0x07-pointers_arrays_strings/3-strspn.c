ab24bb70528bd8964804c5b2e8b79dbf211c696c
bashir <bigbasho@gmail.com>
2022-09-27 01:29:17 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,43 +1,32 @@
 #include "main.h"
 
 /**
- * _strspn - Locates a character in a string
- * @s: This is the main C string to be scanned.
- * @accept: This is the string containing the list of characters to match in s
- * Return: return count
+ * _strspn - a function that gets the length of a prefix substring
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
-	int count = 0;
-	char *str1, *str2;
+	unsigned int i, n, value, check;
 
-	str1 = s;
-	str2 = accept;
+	value = 0;
 
-	i = 0;
-	while (str1[i] != '\0') /*Declaring WHILE *s */
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		j = 0;
-		while (str2[j] != '\0') /*Declaring WHILE *accept*/
+		check = 0;
+
+		for (n = 0; accept[n] != '\0'; n++)
 		{
-			if (str2[j] == str1[i]) /*Evaluate condition*/
+			if (accept[n] == s[i])
 			{
-				count++; /*count number*/
-				break;
+				value++;
+				check = 1;
 			}
-
-			j++;    /*add j+1*/
 		}
-
-		if (s[i] != accept[j]) /*If aren't equals*/
-
-			break;
-		}
-
-		i++; /*add x+1*/
+		if (check == 0)
+			return (value);
 	}
 
-	return (count); /*return the value of count*/
+	return (value);
 }
